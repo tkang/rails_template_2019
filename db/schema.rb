12bd3ae1d0f2339002aa69e936f792fec9e5cabc
Taeho Kang <tkang1@gmail.com>
2018-09-06 23:56:38 +0900

    shoulda-matchers gem; sample rspec with shoulda-matchers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906055946) do
+ActiveRecord::Schema.define(version: 20180906145025) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
