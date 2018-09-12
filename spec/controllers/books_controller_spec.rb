e6f2306340c6f31f9ff84ee45d2ee45e345265c0
Taeho Kang <tkang1@gmail.com>
2018-09-12 19:16:11 +0900

    refactor tests

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,123 +1,125 @@
 require 'rails_helper'
 
 RSpec.shared_examples "should redirect to login page" do
-  it { expect(subject.status).to be(302) }
-  it { expect(subject).to redirect_to(new_user_session_path) }
+	it { expect(subject.status).to be(302) }
+	it { expect(subject).to redirect_to(new_user_session_path) }
 end
 
 RSpec.shared_examples "should redirect to root page" do
-  it { expect(subject.status).to be(302) }
-  it { expect(subject).to redirect_to(root_path) }
+	it { expect(subject.status).to be(302) }
+	it { expect(subject).to redirect_to(root_path) }
 end
 
 RSpec.shared_examples "should render show page" do
-  it { expect(subject.status).to be(200) }
-  it { expect(subject).to render_template(:show) }
+	it { expect(subject.status).to be(200) }
+	it { expect(subject).to render_template(:show) }
+end
+
+RSpec.shared_examples "render index page" do
+	it { expect(subject.status).to be(200) }
+	it { expect(subject).to render_template(:index) }
 end
 
 RSpec.describe BooksController, "GET #index" do
-  let(:user) { FactoryBot.create(:user) }
-  let!(:book) { FactoryBot.create(:book) }
-  subject { get :index }
+	let(:user) { FactoryBot.create(:user) }
+	let!(:book) { FactoryBot.create(:book) }
+	subject { get :index }
 
-  context "if logged in" do
-    before { sign_in(user) }
+	context "if logged in" do
+		before { sign_in(user) }
 
-    it do
-      expect(subject.status).to be(200)
-      expect(subject).to render_template(:index)
-    end
+		it_behaves_like "render index page"
 
-    it do
-      subject
-      expect(assigns(:books)).to eq(Book.all)
-    end
-  end
+		it do
+			subject
+			expect(assigns(:books)).to eq(Book.all)
+		end
+	end
 
-  context "if not logged in" do
-    it_behaves_like "should redirect to login page"
-  end
+	context "if not logged in" do
+		it_behaves_like "should redirect to login page"
+	end
 end
 
 RSpec.describe BooksController, "GET #show" do
-  let(:user) { FactoryBot.create(:user) }
-  let!(:book) { FactoryBot.create(:book) }
-  subject { get :show, params: { id: book.id } }
+	let(:user) { FactoryBot.create(:user) }
+	let!(:book) { FactoryBot.create(:book) }
+	subject { get :show, params: { id: book.id } }
 
-  context "if logged in" do
-    before { sign_in(user) }
+	context "if logged in" do
+		before { sign_in(user) }
 
-    it_behaves_like "should render show page"
+		it_behaves_like "should render show page"
 
-    it do
-      subject
-      expect(assigns(:book)).to eq(book)
-    end
-  end
+		it do
+			subject
+			expect(assigns(:book)).to eq(book)
+		end
+	end
 
-  context "if not logged in" do
-    it_behaves_like "should redirect to login page"
-  end
+	context "if not logged in" do
+		it_behaves_like "should redirect to login page"
+	end
 end
 
 RSpec.describe BooksController, "POST #create" do
-  let(:user) { FactoryBot.create(:user) }
-  let(:admin_user) { FactoryBot.create(:admin_user) }
+	let(:user) { FactoryBot.create(:user) }
+	let(:admin_user) { FactoryBot.create(:admin_user) }
 
-  subject { post :create, params: { book: { title: 'book1' } } }
+	subject { post :create, params: { book: { title: 'book1' } } }
 
-  context "if logged in with normal user" do
-    before { sign_in(user) }
+	context "if logged in with normal user" do
+		before { sign_in(user) }
 
-    it_behaves_like "should redirect to root page"
-  end
+		it_behaves_like "should redirect to root page"
+	end
 
-  context "if logged in with admin user" do
-    before { sign_in(admin_user) }
+	context "if logged in with admin user" do
+		before { sign_in(admin_user) }
 
-    it_behaves_like "should render show page"
+		it_behaves_like "should render show page"
 
-    it 'creates a book' do
-      subject
-      expect(assigns(:book)).to eq(Book.last)
-      expect(Book.last.user).to eq(admin_user)
-    end
-  end
+		it 'creates a book' do
+			subject
+			expect(assigns(:book)).to eq(Book.last)
+			expect(Book.last.user).to eq(admin_user)
+		end
+	end
 
-  context "if not logged in" do
-    it_behaves_like "should redirect to login page"
-  end
+	context "if not logged in" do
+		it_behaves_like "should redirect to login page"
+	end
 end
 
 RSpec.describe BooksController, "DELETE #destroy" do
-  let(:user) { FactoryBot.create(:user) }
-  let(:admin_user) { FactoryBot.create(:admin_user) }
-  let!(:book) { FactoryBot.create(:book) }
+	let(:user) { FactoryBot.create(:user) }
+	let(:admin_user) { FactoryBot.create(:admin_user) }
+	let!(:book) { FactoryBot.create(:book) }
 
-  subject { delete :destroy, params: { id: book.id } }
+	subject { delete :destroy, params: { id: book.id } }
 
-  context "if logged in with normal user" do
-    before { sign_in(user) }
+	context "if logged in with normal user" do
+		before { sign_in(user) }
 
-    it_behaves_like "should redirect to root page"
-  end
+		it_behaves_like "should redirect to root page"
+	end
 
-  context "if logged in with admin user" do
-    before { sign_in(admin_user) }
+	context "if logged in with admin user" do
+		before { sign_in(admin_user) }
 
-    it { expect(subject.status).to be(302) }
-    it { expect(subject).to redirect_to(books_path) }
+		it { expect(subject.status).to be(302) }
+		it { expect(subject).to redirect_to(books_path) }
 
-    it 'deletes a book' do
-      subject
-      expect(assigns(:book)).to eq(book)
-      expect(Book.find_by(id: book.id)).to be_nil
-    end
-  end
+		it 'deletes a book' do
+			subject
+			expect(assigns(:book)).to eq(book)
+			expect(Book.find_by(id: book.id)).to be_nil
+		end
+	end
 
-  context "if not logged in" do
-    it_behaves_like "should redirect to login page"
-  end
+	context "if not logged in" do
+		it_behaves_like "should redirect to login page"
+	end
 end
 
 
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 require 'cancan/matchers'
 
+RSpec.shared_examples "guest user" do
+  it { should_not be_able_to(:manage, :all) }
+end
+
+RSpec.shared_examples "normal user" do
+  it { should_not be_able_to(:manage, :all) }
+  it { should be_able_to(:read, Book) }
+end
+
+RSpec.shared_examples "admin user" do
+  it { should be_able_to(:manage, :all) }
+end
 
 describe User do
   subject(:ability) { Ability.new(user) }
   let(:user) { nil }
 
   context "for guest user" do
-    it { should_not be_able_to(:manage, :all) }
+    it_behaves_like "guest user"
   end
 
   context "for logged in user" do
     let(:user) { FactoryBot.create(:user) }
-    it { should_not be_able_to(:manage, :all) }
-    it { should be_able_to(:read, Book) }
+
+    it_behaves_like "normal user"
 
     context "for admin user" do
       let(:user) { FactoryBot.create(:admin_user) }
-
-      it { should be_able_to(:manage, :all) }
+      it_behaves_like "admin user"
     end
   end
 end
